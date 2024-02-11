1273351be1ae3998810144fa5775a08b45a7b124
x038xx77 <x038xx77@gmail.com>
2024-02-11 22:36:46 +0800

    new

---
+++ app/src/main/java/com/example/appblethermalprinterjava/ConnectionClass.java
@@ -0,0 +1,10 @@
+package com.example.appblethermalprinterjava;
+
+public class ConnectionClass {
+    private static final String printer_name = "";
+
+    public String getPrinterName() {
+        return printer_name;
+    }
+
+}
--- app/src/main/java/com/example/appblethermalprinterjava/MainActivity.java
+++ app/src/main/java/com/example/appblethermalprinterjava/MainActivity.java
@@ -1,87 +1,272 @@
 package com.example.appblethermalprinterjava;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.Method;
+import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
+    private boolean btPermission = false;
+    private BluetoothAdapter bluetoothAdapter;
+    private BluetoothSocket socket;
+    private BluetoothDevice bluetoothDevice;
+    private OutputStream outputStream;
+    private InputStream inputStream;
+    private Thread workerThread;
+    private byte[] readBuffer;
+    private int readBufferPosition = 0;
 
-    private BluetoothController bluetoothController;
-
-    private final Handler handler = new Handler(new Handler.Callback() {
-        @Override
-        public boolean handleMessage(Message msg) {
-            showToast((String) msg.obj);
-            return true;
-        }
-    });
+    private volatile boolean stopWorker = false;
+    private String value = "";
+    private ConnectionClass connectionClass = new ConnectionClass();
+    private BluetoothDevice selectedDevice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        bluetoothController = new BluetoothController(handler);
-        Button onOffButton = findViewById(R.id.onOffButton);
-        Button searchButton = findViewById(R.id.searchButton);
-        Button connectButton = findViewById(R.id.connectButton);
-        Button startButton = findViewById(R.id.startButton);
+    public void scanBt(View view) {
+        checkPermission();
+    }
 
-        onOffButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (bluetoothController.isBluetoothEnabled()) {
-                    // Запускаем BluetoothController при нажатии на кнопку поиска
-                    Intent bluetoothIntent = new Intent(MainActivity.this, BluetoothControllerActivity.class);
-                    startActivity(bluetoothIntent);
-                } else {
-                    showToast("Bluetooth is not enabled");
+    public void print(View view) {
+        if (btPermission) {
+            print_inv();
+        } else {
+            checkPermission();
+        }
+    }
+
+    private void checkPermission() {
+        BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
+        BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
+
+        if (bluetoothAdapter == null) {
+            // Bluetooth не поддерживается на устройстве
+        } else {
+            if (bluetoothAdapter.isEnabled()) {
+                // Bluetooth включен, продолжайте с вашей логикой
+                // например, вызов btScan()
+                btScan();
+            } else {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                btActivityResultLauncher.launch(enableBtIntent);
+            }
+        }
+    }
+
+    private volatile ActivityResultLauncher<String> bluetoothPermissionLauncher = registerForActivityResult(
+            new ActivityResultContracts.RequestPermission(),
+            new ActivityResultCallback<Boolean>() {
+                @Override
+                public void onActivityResult(Boolean isGranted) {
+                    if (isGranted) {
+                        BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+                        BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
+                        btPermission = true;
+
+                        if (bluetoothAdapter != null && !bluetoothAdapter.isEnabled()) {
+                            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                            btActivityResultLauncher.launch(enableBtIntent);
+                        }
+                    } else {
+                        // Действия при отказе в получении разрешения Bluetooth
+                        btScan();
+                    }
                 }
             }
-        });
+    );
 
-        searchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (bluetoothController.isBluetoothEnabled()) {
-                    // Запускаем BluetoothDiscoveryActivity при нажатии на кнопку поиска
-                    Intent discoveryIntent = new Intent(MainActivity.this, BluetoothDiscoveryActivity.class);
-                    startActivity(discoveryIntent);
-                } else {
-                    showToast("Bluetooth is not enabled");
+    private ActivityResultLauncher<Intent> btActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    btScan();
                 }
             }
-        });
+    );
 
+    @SuppressLint("MissingPermission")
+    private void btScan() {
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
+            // Bluetooth выключен или отсутствует
+            // Вы можете добавить обработку включения Bluetooth здесь
+            return;
+        }
 
-        connectButton.setOnClickListener(new View.OnClickListener() {
+        Intent discoverDevicesIntent = new Intent(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        sendBroadcast(discoverDevicesIntent);
+
+        BroadcastReceiver receiver = new BroadcastReceiver() {
             @Override
-            public void onClick(View view) {
-                Log.d(TAG, "Connect button clicked");
-                // Дополнительный код для кнопки "Подключить"
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    String deviceName = device.getName();
+                    String deviceAddress = device.getAddress();
+                    Log.d("Bluetooth", "Device Found: " + deviceName + " (" + deviceAddress + ")");
+                    // Отобразите найденные устройства в списке
+                    displayDevice(device);
+                } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                    unregisterReceiver(this);
+                }
             }
-        });
+        };
 
-        startButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d(TAG, "Start button clicked");
-                // Дополнительный код для кнопки "Старт"
-            }
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(receiver, filter);
+
+        bluetoothAdapter.startDiscovery();
+    }
+
+    private void displayDevice(final BluetoothDevice device) {
+        // Отобразите найденные устройства в списке
+        ListAdapter adapter = new ArrayAdapter<>(this, R.layout.item_list, R.id.item_name,
+                new String[]{device.getName()});
+        ListView listView = findViewById(R.id.bt_lst);
+        listView.setAdapter(adapter);
+
+        // Добавьте листенер для элементов списка
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            selectedDevice = device;
+            EditText deviceNameEditText = findViewById(R.id.deviceName);
+            deviceNameEditText.setText(device.getName());
         });
     }
 
-    private void showToast(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    private void beginListenForData() {
+        try {
+            stopWorker = false;
+            readBufferPosition = 0;
+            byte[] readBuffer = new byte[1024];
+
+            workerThread = new Thread(() -> {
+                while (!Thread.currentThread().isInterrupted() && !stopWorker) {
+                    try {
+                        int bytesAvailable = inputStream.available();
+                        if (bytesAvailable > 0) {
+                            // Ваш код для обработки доступных байтов
+                        }
+                    } catch (IOException e) {
+                        stopWorker = true;
+                    }
+                }
+            });
+
+            workerThread.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initPrinter() {
+        try {
+            String prname = connectionClass.getPrinterName();
+            BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
+            BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
+
+            if (!bluetoothAdapter.isEnabled()) {
+                Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                btActivityResultLauncher.launch(enableBluetooth);
+            }
+
+            Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+            if (pairedDevices != null && pairedDevices.size() > 0) {
+                for (BluetoothDevice device : pairedDevices) {
+                    if (device.getName().equals(prname)) {
+                        bluetoothDevice = device;
+                        UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+                        Method m = bluetoothDevice.getClass().getMethod("createRfcommSocket", int.class);
+                        socket = (BluetoothSocket) m.invoke(bluetoothDevice, 1);
+                        bluetoothAdapter.cancelDiscovery();
+                        socket.connect();
+                        outputStream = socket.getOutputStream();
+                        inputStream = socket.getInputStream();
+                        beginListenForData();
+                        break;
+                    }
+                }
+            } else {
+                value = "No Devices found";
+                Toast.makeText(this, value, Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Bluetooth Printer Not Connected", Toast.LENGTH_LONG).show();
+            socket = null;
+        }
+    }
+
+    private void print_inv() {
+        try {
+            String invhdr = "Tax Invoice";
+            StringBuilder textData = new StringBuilder();
+
+            if (!invhdr.isEmpty()) {
+                textData.append(invhdr);
+            }
+            intentPrint(String.valueOf(textData));
+
+        } catch (Exception e) {
+            Toast.makeText(this, "Error printing invoice", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void intentPrint(String txtValue) {
+        if (!connectionClass.getPrinterName().trim().isEmpty()) {
+            initPrintProcess();
+        }
+    }
+
+    private void initPrintProcess() {
+        try {
+            initPrinter();
+
+            byte[] printHeader = new byte[]{(byte) 0xAA, 0, 0, 0};
+
+            if (printHeader.length > 128) {
+                value += "\nValue is more than 128 size\n";
+                Toast.makeText(this, "Error printing invoice", Toast.LENGTH_LONG).show();
+            } else {
+                outputStream.write("Text TEST Print".getBytes());
+                outputStream.flush();
+                outputStream.close();
+                socket.close();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Error printing invoice", Toast.LENGTH_LONG).show();
+        }
     }
 }
-
